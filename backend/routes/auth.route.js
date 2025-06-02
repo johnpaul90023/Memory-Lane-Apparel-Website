dfd396d64c8306882f1cdb31aa6423282690e993
johnpaul90023 <114263764+johnpaul90023@users.noreply.github.com>
2025-06-02 21:55:59 +0400

    Fix: corrected route paths to avoid missing parameter error

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {login, logout, signup, refreshToken, getProfile} from "../controllers/auth.controller.js";
+import { login, logout, signup, refreshToken, getProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -7,9 +7,7 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-router.post("/refresh-token", refreshToken)
-router.get("/profile", protectRoute, getProfile)
+router.post("/refresh-token", refreshToken);
+router.get("/profile", protectRoute, getProfile);
 
-
-//router.post("/profile", getProfile);
 export default router;
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,13 +1,21 @@
 import express from "express";
-import {toggleFeaturedProduct,getProductsByCategory, getRecommendedProducts, deleteProduct, createProduct, getAllProducts, getFeaturedProducts} from "../controllers/product.controller.js";
-import {adminRoute, protectRoute} from "../middleware/auth.middleware.js";
+import {
+	createProduct,
+	deleteProduct,
+	getAllProducts,
+	getFeaturedProducts,
+	getProductsByCategory,
+	getRecommendedProducts,
+	toggleFeaturedProduct,
+} from "../controllers/product.controller.js";
+import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
-router.get("/recommendations", getRecommendedProducts);
 router.get("/category/:category", getProductsByCategory);
+router.get("/recommendations", getRecommendedProducts);
 router.post("/", protectRoute, adminRoute, createProduct);
 router.patch("/:id", protectRoute, adminRoute, toggleFeaturedProduct);
 router.delete("/:id", protectRoute, adminRoute, deleteProduct);
