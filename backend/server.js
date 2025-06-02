2f12a391581d68a71bdc49035506dd7cae06e6a0
johnpaul90023 <114263764+johnpaul90023@users.noreply.github.com>
2025-06-02 21:04:20 +0400

    Fix route parameter error in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
-import cookieParser  from "cookie-parser";
+import cookieParser from "cookie-parser";
 import path from "path";
+
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
-import couponsRoutes from "./routes/coupon.route.js";
-import paymentsRoutes from "./routes/payment.route.js";
+import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
 import analyticsRoutes from "./routes/analytics.route.js";
 
-import {connectDB} from "./lib/db.js";
+import { connectDB } from "./lib/db.js";
 
-dotenv.config({limit: "10mb"});
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
-app.use("/api/coupons", couponsRoutes);
-app.use("/api/payments", paymentsRoutes);
+app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
 if (process.env.NODE_ENV === "production") {
@@ -36,10 +37,7 @@ if (process.env.NODE_ENV === "production") {
 	});
 }
 
-app.listen(5000, () => {
-    console.log("Server is running on http://localhost:" + PORT);
-
-    connectDB();
+app.listen(PORT, () => {
+	console.log("Server is running on http://localhost:" + PORT);
+	connectDB();
 });
-
-// 00mvdHIh7CCxg6Ov
