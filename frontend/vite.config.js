61a288fbb74d52ba5b08521b63b2bfdc2fee2630
johnpaul90023 <114263764+johnpaul90023@users.noreply.github.com>
2025-06-03 00:04:58 +0400

    Add manualChunks config to vite.config.js to split vendor chunk

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,23 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-			},
-		},
-	},
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // separate chunk for all node_modules code
+          }
+        },
+      },
+    },
+  },
 });
